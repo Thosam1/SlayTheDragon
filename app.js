7408d4d9d151a4b7dbb0a91ee03302d519fe0db4
Thosam1 <thosamtenzin2002@gmail.com>
2021-08-13 14:37:06 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const square = document.querySelectorAll('.square')     // linking to css using .
+const mole = document.querySelectorAll('.mole')
+const timeLeft = document.querySelectorAll('#time-left')       // linking to html using #
+
+let score = document.querySelector('#score') // using 'let' because it will change
+
+// Start of the game
+let result = 0
+function randomSquare() {
+    square.forEach(className => {
+        className.classList.remove
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
