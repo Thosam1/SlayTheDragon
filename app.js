75b41aea046e902255e3d6e1d020a7d9bf9fa3d6
Thosam1 <thosamtenzin2002@gmail.com>
2021-08-13 15:18:16 +0200

    working 3x3 grid

--- app.js
+++ app.js
@@ -1,32 +1,57 @@
-const square = document.querySelectorAll('.square')     // linking to css using .
-const mole = document.querySelectorAll('.mole')
-const timeLeft = document.querySelectorAll('#time-left')       // linking to html using #
+const squares = document.querySelectorAll('.square')     // linking to css using .
+const mole = document.querySelector('.mole')
+const timeLeft = document.querySelector('#time-left')       // linking to html using #
 
 let score = document.querySelector('#score') // using 'let' because it will change
 
 // Start of the game
 let result = 0
+let hitPosition
+let currentTime = 60
+let timerId = null
+
+
+
 function randomSquare() {
-    square.forEach(className => {
-        className.classList.remove
+
+    squares.forEach(className => {
+        className.classList.remove('mole') // we remove the mole from all squares (if they do not contain it, it passes)
     })
+
+    let rdmPosition = squares[Math.floor(Math.random() * 9)] // random index 0-8
+    rdmPosition.classList.add('mole') //adding the mole to that position
+
+    // assigning the id of the rdm position to the hitPosition
+    hitPosition = rdmPosition.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if(square.id === hitPosition){
+            result++
+            score.textContent = result; // showing it via the html file
+            hitPosition = null
+        }
+    })
+})
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+}
+
+moveMole()
+
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+
+    if(currentTime == 0) {
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert("Time's Up ! Your final score is : " + result)
+    }
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let countDownTimerId = setInterval(countDown, 1000)
